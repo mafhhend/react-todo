3dfd734ce05527a6e6cb3174ee48a3f931c0c63c
saman <mafhh.end@gmail.com>
2022-05-20 19:45:00 +0430

    use onBlur to send request after focus out the input

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,20 +1,18 @@
-import { Fragment, useContext, useState } from "react"
+import { Fragment, useContext, useEffect, useState } from "react"
 import TodoContext from "../../store/TodoContext"
 const TodoItem = ({ todo }) => {
 
     const { deleteTodo, updateTodo } = useContext(TodoContext)
     const [todoText, setTodoText] = useState(todo.title);
     const [isShowInput, setIsShowInput] = useState(false)
+
     const updateTodoText = e => {
         setTodoText(e.target.value)
-
-        setTimeout(() => {
-            updateTodo(todo.id, e.target.value)
-        }, 3000);
     }
+
     return <Fragment>
         <div className="flex items-center justify-between border-b">
-            {isShowInput && <input type="text" className="p-2 border-2 rounded" value={todoText} onChange={updateTodoText} />}
+            {isShowInput && <input type="text" className="p-2 border-2 rounded" value={todoText} onChange={updateTodoText} onBlur={e => updateTodo(todo.id, todoText)} />}
             {!isShowInput && <span>{todo.title}</span>}
             <div>
                 <button className="m-2 btn-base btn-dark" onClick={() => setIsShowInput(!isShowInput)}>Edit</button>
